e7e7fd676512a5dbe44538f2f46f2009e2b74f82
jayraj2498 <jayraj2498@gmail.com>
2024-04-03 17:25:04 +0530

    artifacts file deleted

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -26,11 +26,11 @@ if __name__=="__main__":
     obj=DataIngestion()
     train_data ,test_data =obj.initiate_data_ingestion() 
     
-    data_transformation=DataTransformation() 
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)  # we are giving data 
+    # data_transformation=DataTransformation() 
+    # train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)  # we are giving data 
 
-    modeltrainer=ModelTrainer()
-    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))  
+    # modeltrainer=ModelTrainer()
+    # print(modeltrainer.initiate_model_trainer(train_arr,test_arr))  
     
     
     
